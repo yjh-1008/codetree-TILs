d8c5b14b1890da3a6d7bf4c4230f5e79fd38d6de
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-04-13 14:51:33 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240413/황금비율 토스트/golden-toast.js
+++ 240413/황금비율 토스트/golden-toast.js
@@ -1,132 +1,139 @@
 class Node {
-  constructor(data) {
-      this.data = data;
-      this.next = null;
-      this.prev = null;
-  }
-}
-
-class DoubleLinkedList {
-  constructor() {
-      this.END = new Node(-1);
-      this.head = this.END;
-      this.tail = this.END;
-      this.size = 0;
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
     }
-
-  pushFront(data) {
-      const newNode = new Node(data);
-      newNode.next = this.head;
-
-      this.head.prev = newNode;
-      this.head = newNode;
-      newNode.prev = null;
-      this.size += 1;
   }
-
-  pushBack(data) {
-      const newNode = new Node(data);
-      if(this.begin() === this.end()) {
-          this.pushFront(data);
-      }else {
-          newNode.prev = this.tail.prev;
-          this.tail.prev.next = newNode;
-          newNode.next = this.tail;
-          this.tail.prev = newNode;
-
-          this.size += 1;
+  
+  class DoubleLinkedList {
+    constructor() {
+        this.END = new Node(-1);
+        this.head = this.END;
+        this.tail = this.END;
+        this.size = 0;
       }
-  }
-
-  erase(node) {
-      const nextNode = node.next;
-
-      if(node === this.begin()) {
-          const temp = this.head;
-          temp.next.prev = null;
-          this.head = temp.next;
-          temp.next=null;
-      } else {
-          node.prev.next = node.next;
-          node.next.prev =node.prev;
-          node.next = null;
-          node.prev = null;
-      }
-
-      this.size -= 1;
-      return nextNode;
-  }
-
-  insert(node, newData) {
-      if(node === this.end()) {
-          this.pushBack();
-      }else if(node ===this.begin()) {
-          this.pushFront();
-      }else {
-          const newNode = new Node(newData);
-          newNode.prev = node.prev;
-          newNode.next = node;
-          node.prev.next = newNode;
-          node.prev = newNode;
-
-          this.size += 1;
-      }
-  }
-
-  begin() {
-      return this.head;
-  }
-
-  end() {
-      return this.tail;
-  }
-}
-
-const fs = require('fs');
-const input= fs.readFileSync('/dev/stdin').toString().trim().split("\n");
-let [N, M] = input[0].split(" ").map(Number);
-const arr = input[1].split("");
-const Solution = () => {
-    let idx = arr.length;
-    const doubleList = new DoubleLinkedList();
-    arr.forEach((item) => {
-        doubleList.pushBack(item);
-    })
-    for(let i=2;i<input.length;i++) {
-        const [cmd, str] = input[i].split(" ");
-        let temp = null;
-        switch(cmd) {
-            case 'L':
-                if(idx < 0) continue;
-                idx -= 1;
-                break;
-            case 'P':
-                temp = doubleList.head;
-                for(let i=0;i<idx;i++) {
-                    temp = temp.next;
-                }
-                doubleList.insert(temp, str);
-                break;
-            case 'R':
-                if(idx > M) continue;
-                idx += 1;
-                break;
-            case 'D':
-                temp = doubleList.head;
-                for(let i=0;i<=idx;i++) {
-                    temp = temp.next;
-                }
-                doubleList.erase(temp);
-                break;
+  
+    pushFront(data) {
+        const newNode = new Node(data);
+        newNode.next = this.head;
+  
+        this.head.prev = newNode;
+        this.head = newNode;
+        newNode.prev = null;
+        this.size += 1;
+    }
+  
+    pushBack(data) {
+        const newNode = new Node(data);
+        if(this.begin() === this.end()) {
+            this.pushFront(data);
+        }else {
+            newNode.prev = this.tail.prev;
+            this.tail.prev.next = newNode;
+            newNode.next = this.tail;
+            this.tail.prev = newNode;
+  
+            this.size += 1;
         }
     }
-    let temp = doubleList.head;
-    const ret= [];
-    while(temp != doubleList.END) {
-        ret.push(temp.data)
-        temp = temp.next
+  
+    erase(node) {
+        const nextNode = node.next;
+  
+        if(node === this.begin()) {
+            const temp = this.head;
+            temp.next.prev = null;
+            this.head = temp.next;
+            temp.next=null;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev =node.prev;
+            node.next = null;
+            node.prev = null;
+        }
+  
+        this.size -= 1;
+        return nextNode;
     }
-    console.log(ret.join(""))
-}
-
-Solution();
+  
+    insert(node, newData) {
+        if(node === this.end()) {
+            this.pushBack();
+        }else if(node ===this.begin()) {
+            this.pushFront();
+        }else {
+            const newNode = new Node(newData);
+            newNode.prev = node.prev;
+            newNode.next = node;
+            node.prev.next = newNode;
+            node.prev = newNode;
+  
+            this.size += 1;
+        }
+    }
+  
+    begin() {
+        return this.head;
+    }
+  
+    end() {
+        return this.tail;
+    }
+  }
+  
+  const fs = require('fs');
+  const input= fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+  let [N, M] = input[0].split(" ").map(Number);
+  const arr = input[1].split("");
+  const Solution = () => {
+      let idx = arr.length-1;
+      const doubleList = new DoubleLinkedList();
+      arr.forEach((item) => {
+          doubleList.pushBack(item);
+      })
+      for(let i=2;i<input.length;i++) {
+          const [cmd, str] = input[i].split(" ");
+          let temp = null;
+          switch(cmd) {
+              case 'L':
+                  if(idx <= 0) continue;
+                  idx -= 1;
+                  break;
+              case 'P':
+                  temp = doubleList.head;
+                  for(let i=0;i<=idx;i++) {
+                      temp = temp.next;
+                  }
+                  idx+=1;
+                  doubleList.insert(temp, str);
+                  break;
+              case 'R':
+                  if(idx > M) continue;
+                  idx += 1;
+                  break;
+              case 'D':
+                  temp = doubleList.head;
+                  if(idx == 0) doubleList.erase(temp);
+                  else if(idx === M) continue;
+                  else {
+                    for(let i=0;i<idx;i++) {
+                        temp = temp.next;
+                    }
+                    // console.log(temp);
+                    doubleList.erase(temp.next);
+                  }
+                  
+                  break;
+          }
+      }
+      let temp = doubleList.head;
+      const ret= [];
+      while(temp != doubleList.END) {
+          ret.push(temp.data)
+          temp = temp.next
+      }
+      console.log(ret.join(""))
+  }
+  
+  Solution();
