f4b50f60835416021cc4111cf11c29e02208d2fb
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-04-15 01:40:44 +0900

    Title: 기수 정렬 구현, Time: 333ms, Memory: 43MB, Status: Passed - CodeTree

--- 240415/기수 정렬 구현/implement-radix-sort.js
+++ 240415/기수 정렬 구현/implement-radix-sort.js
@@ -1,28 +1,40 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+const input = fs.readFileSync(0).toString().trim().split("\n");
 const N = parseInt(input[0]);
 const arr1 = input[1].split(" ").map(Number);
-const len = arr1.reduce((maxLen, item) => Math.max(maxLen, item.toString().length),0)
+const len = mostDigits(arr1);
 
 function getDigit(num,i) {
 	return Math.floor(Math.abs(num) / Math.pow(10, i) % 10);
 }
 
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
 const radix_sort = (arr, k) => { //k는 자릿수
-    for(let pos = k-1; pos>=0; pos--) {
-        const arr_new = Array.from({length: 10}, () => new Array());
+    for(let pos = 0; pos<k;pos++) {
+        const arr_new = Array.from({length: 10}, () => []);
         for(let i=0;i<arr.length;i++) {
             const digit = getDigit(arr[i], pos);
             arr_new[digit].push(arr[i]);
         }
         const store_arr = [];
-        // console.log(arr_new)
         for(let i=0;i<10;i++){
-            arr_new[i].forEach((item) => {
-                store_arr.push(item);
-        })
+            for(let j=0;j<arr_new[i].length;j++) {
+              store_arr.push(arr_new[i][j])
+            }
         }
-        
+        // console.log( store_arr);
         arr = store_arr;
     }
     console.log(arr.join(" "));
