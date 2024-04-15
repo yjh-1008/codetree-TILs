a1897a4f9506c66f168b6972f83d7ba4e56f7840
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-04-16 01:23:28 +0900

    Title: 퀵 정렬 구현, Time: 530ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240416/퀵 정렬 구현/implement-quick-sort.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const N = parseInt(input[0]);
+const nums = input[1].split(" ").map(Number);
+
+const pivot_sort =(arr) => {
+  if(arr.length < 2) {
+    return arr;
+  }
+
+  const pivot = [arr[0]];
+  const left = [];
+  const right = [];
+
+  for(let i=1;i<arr.length;i++) {
+    if(arr[i] < pivot) {
+      left.push(arr[i]);
+    } else if(arr[i] > pivot) {
+      right.push(arr[i]);
+    } else{
+      pivot.push(arr[i]);
+    }
+  }
+
+  return pivot_sort(left).concat(pivot, pivot_sort(right));
+}
+
+console.log(pivot_sort(nums).join(" "));
