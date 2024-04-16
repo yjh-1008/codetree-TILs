7159ef88e3f26ebdf82d6f3b5a8a3dc9eeb8fbb7
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-04-17 00:15:43 +0900

    Title: 힙 정렬 구현, Time: 371ms, Memory: 28MB, Status: Passed - CodeTree

--- 240417/힙 정렬 구현/implement-heap-sort.js
+++ 240417/힙 정렬 구현/implement-heap-sort.js
@@ -3,70 +3,45 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 const N = parseInt(input[0]);
 let nums = input[1].split(" ").map(Number);
 
-function heapSort(array) {
-  /**
-   * 요소 swap 메서드
-   * @param {array} swapArray : swap 대상 배열
-   * @param {number} a : 교체 대상 인덱스
-   * @param {number} b : 교체 인덱스
-   */
-  function swap(swapArray, a, b) {
-    const temp = swapArray[a];
-    swapArray[a] = swapArray[b];
-    swapArray[b] = temp;
+function swap(arr, i, j){
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function heapify(arr, i){
+  let max = i;
+  let left = 2 * i + 1;
+  let right = 2 * i + 2;
+  
+  if(left < arrLen && arr[left] > arr[max]){
+    max = left;
   }
-
-  /**
-   * heapify 메서드
-   * @param {array} heapArray : heap으로 변환 할 배열
-   * @param {number} length : 배열 길이
-   * @param {number} parent : 부모 인덱스
-   */
-  function heap(heapArray, length, parent) {
-    const left = parent * 2 + 1;
-    const right = left + 1;
-    let root = parent;
-    //좌측 요소가 부모 값 보다 크면 부모 인덱스를 left 인덱스로 대입
-    if (left < length && heapArray[left] > heapArray[root]) {
-      root = left;
-    }
-
-    //우측 요소가 부모 값 보다 크면 부모 인덱스를 right 인덱스로 대입
-    if (right < length && heapArray[right] > heapArray[root]) {
-      root = right;
-    }
-
-    //부모 요소가 자식 요소와 바꾸어 졌으면 swap처리 및 heap 재검사
-    if (root !== parent) {
-      swap(heapArray, parent, root);
-    //   heap(heapArray, length, root);
-    }
+  
+  if(right < arrLen && arr[right] > arr[max]){
+    max = right;
   }
-
-  /**
-   * 정렬 메서드
-   * @param {array} tempArray : 정렬 대상 배열
-   * @returns
-   */
-  function sort(tempArray) {
-    let length = tempArray.length;
-    if (length === 1) {
-      return tempArray;
-    } else {
-      //배열을 이등분하여 좌측 요소들을 heap 형태로 변환
-      for (let i = Math.floor(tempArray.length / 2) - 1; i >= 0; i--) {
-        heap(tempArray, length, i);
-      }
-
-      for (let i = tempArray.length - 1; i > 0; i--) {
-        swap(tempArray, 0, i); //가장 큰 요소와 정렬되지 않은 마지막 요소와 교환
-        length--;
-        heap(tempArray, length, 0); //배열의 나머지 부분들을 heap상태로 만들며 정렬 과정 수행
-      }
-      return tempArray;
-    }
+  
+  if(max != i){
+    swap(arr, i, max);
+    heapify(arr, max);
   }
+}
 
-  return sort(array);
+function heapSort(arr){
+  arrLen = arr.length;
+
+  for(let i = Math.floor(arrLen / 2); i >= 0; i--){
+    heapify(arr, i);
+  }
+  
+  for(let i = arrLen - 1; i > 0; i--){
+    swap(arr, 0, i);
+    arrLen--;
+    
+    heapify(arr, 0);
+  }
+  
+  return arr;
 }
 console.log(heapSort(nums).join(" "))
