2476b03014f6302bec638322caeba50fe01e6a0c
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-04-18 23:49:50 +0900

    Title: 정수 명령 처리, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240418/정수 명령 처리/process-numeric-commands.js
+++ 240418/정수 명령 처리/process-numeric-commands.js
@@ -1,31 +1,32 @@
 class Stack {
-    constructor() {              // 빈 스택 하나를 생성합니다.
-        this.items = [];
-    }
-    
-    push(item) {
-        this.items.push(item);
-    }
+  constructor() {              // 빈 스택 하나를 생성합니다.
+      this.items = [];
+  }
+  
+  push(item) {
+      this.items.push(item);
+  }
 
-    pop() {
-        return this.items.pop();
-    }
+  pop() {
+      return this.items.pop();
+  }
 
-    size() {
-        return this.items.length;
-    }
+  size() {
+      return this.items.length;
+  }
 
-    empty() {
-        return this.items.length === 0 ? 1 : 0;
-    }
+  empty() {
+      return this.items.length === 0 ? 1 : 0;
+  }
 
-    top() {
-        return this.items[this.items.length-1];
-    }
+  top() {
+    console.log(this.items)
+      return this.items[this.items.length-1];
+  }
 
-    join(str) {
-        return this.items.join(str);
-    }
+  join(str) {
+      return this.items.join(str);
+  }
 }
 
 const fs = require('fs');
@@ -33,24 +34,26 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const N = parseInt(input[0]);
 const arr = input.slice(1);
 const Solution = () => {
-    const s = new Stack();
-    const ret = new Stack();
-    // console.log(input)
-    for(let i=0;i<N;i++) {
-        const [cmd, num] = arr[i].split(" ");
+  const s = new Stack();
+  const ret = new Stack();
+  // console.log(input)
+  for(let i=0;i<N;i++) {
+      const [cmd, num] = arr[i].split(" ");
 
-        if(cmd === 'push') {
-            s.push(num);
-        } else if(cmd === 'pop') {
-            ret.push(s.pop());
-        } else if(cmd === 'size') {
-            ret.push(s.size());
-        } else {
-            ret.push(s.empty());
-        }
-    }
+      if(cmd === 'push') {
+          s.push(num);
+      } else if(cmd === 'pop') {
+          ret.push(s.pop());
+      } else if(cmd === 'size') {
+          ret.push(s.size());
+      } else if(cmd === 'top'){
+          ret.push(s.top());
+      } else {
+        ret.push(s.empty())
+      }
+  }
 
-    console.log(ret.join("\n"));
+  console.log(ret.join("\n"));
 }
 
 Solution();
