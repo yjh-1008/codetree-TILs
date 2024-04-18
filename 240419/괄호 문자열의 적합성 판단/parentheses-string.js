9d0e98d141c9f31bc94dc325e6acfba57af95972
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-04-19 01:17:20 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240419/괄호 문자열의 적합성 판단/parentheses-string.js
@@ -0,0 +1,50 @@
+class Stack {
+  constructor() {              // 빈 스택 하나를 생성합니다.
+      this.items = [];
+  }
+  
+  push(item) {
+      this.items.push(item);
+  }
+
+  pop() {
+      return this.items.pop();
+  }
+
+  size() {
+      return this.items.length;
+  }
+
+  empty() {
+      return this.items.length === 0 ? 1 : 0;
+  }
+
+  top() {
+    console.log(this.items)
+      return this.items[this.items.length-1];
+  }
+
+  join(str) {
+      return this.items.join(str);
+  }
+}
+
+const fs = require('fs');
+const arr = fs.readFileSync(0).toString().trim().split("");
+const stack = [];
+let ret = true;
+arr.forEach((item) => {
+    if(item === '(') {
+        satck.push(item);
+    } else {
+        if(stack.empty()) {
+            ret = false;
+            return;
+        } else {
+            stack.pop();
+        }
+    }
+})
+
+if(stack.empty()) console.log(ret ? 'Yes' : 'No');
+else console.log('No')
