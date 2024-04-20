5a3280afc3467c3c8943a9a2ff6bf03f8f66472b
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-04-20 17:13:44 +0900

    Title: 수열 조작, Time: 5061ms, Memory: 45MB, Status: Time Limit Exceed - CodeTree

--- 240420/수열 조작/sequence-manipulation.js
+++ 240420/수열 조작/sequence-manipulation.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const N = parseInt(fs.readFileSync(0).toString().trim());
 
 const Solution = () => {
- const arr = [];
+ let arr = [];
  for(let i=1;i<=N;i++) arr.push(i);
  while(arr.length > 1) {
-    arr.shift();
-    arr.push(arr.shift())
+    arr = arr.slice(1);
+    arr = [...arr.slice(1), arr[0]];
  }
  console.log(arr[0])
 }
