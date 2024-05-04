bd461d00e563cb239aece6b3b9078b428e109236
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-05 02:49:59 +0900

    Title: 금 채굴하기, Time: 190ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240505/금 채굴하기/gold-mining.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const arr = Array.from({length:N}, ()=> new Array(M));
+
+for(let i=1;i<1+N;i++) {
+    arr[i-1] = input[i].split(" ").map(Number);
+}
+
+const MOVE = [[1,0],[-1,0], [1,0],[-1,0]];
+let ret = 0;
+function chkArea(y, x) {
+    let cnt = 0;
+    while(true)  {
+     for(let i=0;i<4;i++) {
+        const [ny, nx] =MOVE[i];
+        let my, mx;
+        if(ny < 0) {
+           my = ny - cnt + y;
+        } else {
+          my= ny+cnt+y;
+        }
+
+        if(nx < 0) {
+            mx = nx - cnt + x;
+        } else {
+            mx = nx+cnt+y;
+        }
+        
+        // console.log(my, mx)
+        if(my < 0 || mx<0 || my>=N || mx>=M) return cnt;
+      }
+      cnt+=1;
+    }
+}
+
+function findCoin(y, x, cnt) {
+
+    let coin = 0;
+    for(let i= y-cnt+1;i<y+cnt-1;i++) {
+        for(let j=x-cnt+1; j< x+cnt-1;j++) {
+            if(arr[i][j]) coin += 1;
+        }
+    }
+    if(arr[y][x-cnt]) coin +=1;
+    if(arr[y][x+cnt]) coin += 1;
+    if(arr[y-cnt][x]) coin += 1;
+    if(arr[y+cnt][x]) coin +=1;
+    return coin;
+}
+
+function Solution() {
+    for(let i=0;i<N;i++) {
+        for(let j=0;j<M;j++) {
+            let cnt = chkArea(i,j);
+            // console.log(cnt);
+            if(cnt >= 1) {
+                const coin = findCoin(i, j, cnt);
+                ret = Math.max(coin, ret);
+            }
+        }
+    }
+    console.log(ret)
+}
+
+Solution();
