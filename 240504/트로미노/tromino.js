343ec8b6915eb8a28117c31a23d7bf1c83aa285e
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-04 17:06:13 +0900

    Title: 트로미노, Time: 254ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240504/트로미노/tromino.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [N, M] = input[0].split(" ").map(Number);
+const arr = Array.from({length:N}, () => new Array(M));
+
+for(let i=1;i<1+N;i++) {
+    arr[i-1] = input[i].split(" ").map(Number);
+}
+
+let ret = 0;
+
+for(let i=0;i<N;i++) {
+    for(let j=0;j<M;j++) {
+        //ㄴ자 검사
+        if(i-1 > 0 && j+1 < N) {
+            const sum = arr[i][j] + arr[i-1][j] + arr[i][j+1];
+            ret = Math.max(ret, sum);
+        }
+
+        if(i-1 > 0 && j-1 > 0) {
+              const sum = arr[i][j] + arr[i-1][j] + arr[i][j-1];
+            ret = Math.max(ret, sum);
+        }
+
+        if(i+1 < N && j-1 > 0) {
+              const sum = arr[i][j] + arr[i+1][j] + arr[i][j-1];
+            ret = Math.max(ret, sum);
+        }
+
+        if(i+1 < N && j+1 < N) {
+              const sum = arr[i][j] + arr[i+1][j] + arr[i][j+1];
+            ret = Math.max(ret, sum);
+        }
+
+        // ㅡ검사
+        if(j-1 > 0 && j+1 <N) {
+            const sum = arr[i][j] + arr[i][j-1] + arr[i][j+1];
+            ret = Math.max(ret, sum);
+        }
+    }
+}
+
+console.log(ret)
