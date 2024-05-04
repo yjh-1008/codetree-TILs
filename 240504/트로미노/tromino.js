4d3bf29c58cfd1f56a4b96b63183687782e1265c
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-04 17:14:03 +0900

    Title: 트로미노, Time: 254ms, Memory: 19MB, Status: Passed - CodeTree

--- 240504/트로미노/tromino.js
+++ 240504/트로미노/tromino.js
@@ -6,39 +6,39 @@ const arr = Array.from({length:N}, () => new Array(M));
 for(let i=1;i<1+N;i++) {
     arr[i-1] = input[i].split(" ").map(Number);
 }
-
+// console.log(arr);
 let ret = 0;
 
 for(let i=0;i<N;i++) {
     for(let j=0;j<M;j++) {
         //ㄴ자 검사
-        if(i-1 > 0 && j+1 < N) {
+        if(i-1 >= 0 && j+1 < M) {
             const sum = arr[i][j] + arr[i-1][j] + arr[i][j+1];
             ret = Math.max(ret, sum);
         }
 
-        if(i-1 > 0 && j-1 > 0) {
+        if(i-1 >= 0 && j-1 >= 0) {
               const sum = arr[i][j] + arr[i-1][j] + arr[i][j-1];
             ret = Math.max(ret, sum);
         }
 
-        if(i+1 < N && j-1 > 0) {
+        if(i+1 < N && j-1 >= 0) {
               const sum = arr[i][j] + arr[i+1][j] + arr[i][j-1];
             ret = Math.max(ret, sum);
         }
 
-        if(i+1 < N && j+1 < N) {
-              const sum = arr[i][j] + arr[i+1][j] + arr[i][j+1];
+        if(i+1 < N && j+1 < M) {
+            const sum = arr[i][j] + arr[i+1][j] + arr[i][j+1];
             ret = Math.max(ret, sum);
         }
 
         // ㅡ검사
-        if(j-1 > 0 && j+1 <N) {
+        if(j-1 >= 0 && j+1 <M) {
             const sum = arr[i][j] + arr[i][j-1] + arr[i][j+1];
             ret = Math.max(ret, sum);
         }
 
-        if(i-1 > 0 && i+1<N) {
+        if(i-1 >= 0 && i+1<N) {
             const sum = arr[i][j] + arr[i-1][j] + arr[i+1][j];
             ret = Math.max(ret, sum);
         }
