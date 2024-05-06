b914ce1bb9ec22cd176e6f79704d7eca7a00e197
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-06 14:31:45 +0900

    Title: 기울어진 직사각형, Time: 254ms, Memory: 21MB, Status: Passed - CodeTree

--- 240506/기울어진 직사각형/slanted-rectangle.js
+++ 240506/기울어진 직사각형/slanted-rectangle.js
@@ -6,52 +6,39 @@ for(let i=1;i<N+1;i++) {
   arr[i-1] = input[i].split(" ").map(Number); 
 }
 
-const chkArea = [[-1,1],[-2,0],[-1,-1]];
+const moves = [[-1,1],[-1,-1],[1,-1],[1,1]];
 
-function isGo(y, x) {
-  for(let i=0;i<3;i++) {
-    const [ny, nx] = chkArea[i];
-    const my = y+ny, mx = x+nx;
-    if(my < 0 || my >= N || mx < 0 || mx >= N) return false;
-  }
+function chkRange(y,x) {
+  if(y < 0 || y >= N || x < 0 || x >= N) return false;
   return true;
 }
 
-function getSum(y, x) {
-  let sum = arr[y][x];
-  chkArea.forEach(([ny, nx]) => {
-    sum +=arr[y+ny][x+nx]
-  })
-  let move = [y-1, x+1];
-  while(true) {
-    let tmpSum = 0;
-    let chk = true;
-    for(let i=0;i<2;i++) {
-      const moveY = move[0] + chkArea[i][0];
-      const moveX = move[1] + chkArea[i][1];
-      if(moveY < 0 || moveY >= N || moveX < 0 || moveX >=N){
-        chk = false;
-      }
-      else tmpSum += arr[moveY][moveX]
-    }
-    if(chk) {
-       sum += tmpSum;
-       move[0] = move[0]-1;
-       move[1] = move[1]+1;
-    }else {
-      break;
+function getScore(i, j, k, l) {
+  const moveNum = [k, l, k, l];
+  let selectSum = 0;
+  for(let q=0;q<4;q++) {
+    for(let w = 0;w<moveNum[q];w++) {
+      const [ny, nx] = moves[q];
+      i += ny;
+      j += nx;
+
+      if(!chkRange(i, j)) return 0;
+
+      selectSum += arr[i][j];
     }
   }
-  return sum;
+
+  return selectSum;
 }
+
 let ret = 0;
 function Solution() {
   for(let i=0;i<N;i++) {
     for(let j=0;j<N;j++) {
-      const chk = isGo(i, j);      
-      if(chk) {
-        const sum = getSum(i,j);
-        ret = Math.max(sum, ret);
+      for(let k=1;k<N;k++) {
+        for(let l = 1;l<N;l++) {
+          ret = Math.max(ret, getScore(i, j, k, l));
+        }
       }
     }
   }
