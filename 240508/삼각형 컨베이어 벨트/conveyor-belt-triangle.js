4b66ca9f81c7a5f2d2fd30d4a6cca0ce50de16b1
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-08 01:42:10 +0900

    Title: 삼각형 컨베이어 벨트, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240508/삼각형 컨베이어 벨트/conveyor-belt-triangle.js
+++ 240508/삼각형 컨베이어 벨트/conveyor-belt-triangle.js
@@ -3,25 +3,28 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 
 let [n, t] = input[0].split(" ").map(Number);
 const arr = [];
-for(let i=1;i<=n;i++) {
+for(let i=1;i<=3;i++) {
     arr[i-1] = input[i].split(" ").map(Number);
 }
 
 function Solution() {
     //t초동안 진행하며,
+    // console.log(arr);
     while(t) {
         let tmp = 0;
-        for(let i=0;i<n;i++) {
+        for(let i=0;i<3;i++) {
             //i가 0인 경우에는 값을 빼기만하고
             if(i === 0) {
                 tmp = arr[i].pop();
-            } else if(i > 0 && i<n-1) {
+            } else if(i === 1) {
                 //0<i<n-1인 경우에는 arr[i].unshift(tmp) arr.pop()
                 arr[i].unshift(tmp);
                 tmp = arr[i].pop();
-            } else {
+            } else if(i===2){
+             
                 //i가 n-1이라면 arr[i].unshift(tmp) arr[0].unsfhit(arr[i].pop())
                 arr[i].unshift(tmp);
+                // console.log(arr[i], i);
                 tmp = arr[i].pop();
                 arr[0].unshift(tmp);
             }
