2b2fb97e258cee30c0a41c9a65e87501f500aac1
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-09 00:15:23 +0900

    Title: 1차원 바람, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240509/1차원 바람/The-1D-wind-blows.js
@@ -0,0 +1,66 @@
+//처음은 L은 stack R은 queue
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [n, m, q] = input[0].split(" ").map(Number);
+const arr = [];
+for(let i=1;i<=n;i++) {
+    arr[i-1] = input[i].split(" ").map(Number)
+}
+const cmds = [];
+for(let i=1+n;i<1+n+q;i++) {
+    cmds.push(input[i]);
+}
+
+function sameTile(arr1, arr2) {
+    for(let i=0;i<m;i++) {
+        if(arr1[i] === arr2[i]) return true;
+    }
+    return false;
+}
+
+function go(col, dirNum, isUp) {
+    if(isUp) {
+        for(let i=col;i>=1;i--) {
+            if(!sameTile(arr[i], arr[i-1])) return;
+            if(dirNum) {
+                arr[i-1].unshift(arr[i-1].pop());
+            } else {
+                arr[i-1].push(arr[i-1].shift());
+            }
+            dirNum = !dirNum;
+        }
+    } else {
+        for(let i=col;i<n-1;i++) {
+            if(!sameTile(arr[i], arr[i+1])) return;
+            if(dirNum) {
+                arr[i+1].unshift(arr[i+1].pop());
+            } else {
+                arr[i+1].push(arr[i+1].shift());
+            }
+            dirNum = !dirNum;
+        }
+    }
+}
+ 
+function Solution() {
+    for(let i=0;i<cmds.length;i++) {
+        const [col, dir] = cmds[i].split(" ");
+        //true이면 stack false면 queue
+        let dirNum = dir === 'L' ? true: false;
+        if(dirNum) {
+            arr[col-1].unshift(arr[col-1].pop());
+        } else {
+            arr[col-1].push(arr[col-1].shift());
+        }
+
+        go(col-1, !dirNum, true);
+        go(col-1, !dirNum, false);
+    }
+    let ret = '';
+    arr.forEach((item) => {
+        ret += item.join(" ")+"\n"
+    })
+    console.log(ret.trim())
+}
+
+Solution();
