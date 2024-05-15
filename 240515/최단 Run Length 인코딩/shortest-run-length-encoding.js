e9a3520562bd507cf13d7e81703c561cfe4cd6fb
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-15 23:27:13 +0900

    Title: 최단 Run Length 인코딩, Time: 224ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240515/최단 Run Length 인코딩/shortest-run-length-encoding.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const str = fs.readFileSync(0).toString().trim().split("");
+const N = str.length;
+
+function zipStr(str) {
+    let cnt = 0;
+    let newStr = [str[0]];
+    str.forEach((s) => {
+        if(newStr.at(-1) === s) {
+            cnt+=1;
+        } else {
+            newStr.push(cnt);
+            cnt=1;
+            newStr.push(s)
+        }
+    })
+    newStr.push(cnt);
+    // console.log(str, newStr);
+    return newStr.length;
+}
+
+function Solution() {
+    let ret = Infinity;
+
+    for(let i=0;i<N;i++) {
+        str.unshift(str.pop());
+        ret = Math.min(zipStr(str), ret);
+    }
+
+    console.log(ret);
+}
+
+Solution();
