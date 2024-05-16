0ad7854fa01b9ac7b479f41c935f19adfc80a9ba
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-16 22:55:43 +0900

    Title: 1차원 젠가, Time: 185ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240516/1차원 젠가/jenga-1d.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const N = Number(input[0]);
+let arr = []
+for(let i=1;i<=N;i++) {
+    arr[i-1] = Number(input[i]);
+}
+arr = arr.reverse()
+function Solution() {
+    for(let i=N+1;i<input.length;i++) {
+        const [s,e] = input[i].trim().split(" ").map(Number);
+        let tmp = [];
+        arr.forEach((item,idx) => {
+            if(idx < s || idx > e) {
+                tmp.push(item);
+            }
+        })
+        arr = tmp;
+    }
+
+    let ret = ''
+    ret += arr.length+'\n';
+    arr.reverse().forEach((item) => ret += item+'\n');
+    console.log(ret)
+}
+
+Solution();
