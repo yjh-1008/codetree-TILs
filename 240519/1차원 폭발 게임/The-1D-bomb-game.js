aae35c3d2ddcb09680c996932bfe4950972f0fc9
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-19 02:02:58 +0900

    Title: 1차원 폭발 게임, Time: 161ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240519/1차원 폭발 게임/The-1D-bomb-game.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [N, M] = input[0].trim().split(" ").map(Number);
+let  arr = [];
+for(let i=1;i<=N;i++){
+    arr.push(Number(input[i]))
+}
+
+function exist() {
+    let cnt = 1;
+    for(let i=1;i<arr.length;i++) {
+        if(arr[i-1]===arr[i]) {
+            cnt+= 1;
+            if(cnt >= M) return true;
+        }else {
+            cnt= 1;
+        }
+    }
+    return false;
+}
+
+function Solution() {
+    while(exist()) {
+        let tmp = [arr[0]];
+        let cnt = 1;
+        for(let i=1;i<arr.length;i++) {
+            if(tmp.at(-1) === arr[i]) {
+                tmp.push(arr[i]);
+                cnt += 1;
+                
+            }else {
+                tmp.push(arr[i]);
+                cnt = 1;
+            }
+            if(cnt == M) {
+                for(let j=0;j<M;j++) {
+                    tmp.pop();
+                }
+                cnt = 0;
+            }
+        }
+        arr = tmp;
+        console.log(`${arr.length}\n${arr.join("\n")}`);
+    }
+    
+}
+
+Solution();
