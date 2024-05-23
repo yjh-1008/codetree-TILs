79ec31aff2b80f6b592ac7593002187202111518
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-23 22:17:07 +0900

    Title: 최적의 십자 모양 폭발, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240523/최적의 십자 모양 폭발/best-cross-shape-bomb.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const N = Number(input[0]);
+const arr = input.slice(1, input.length).map((item) => item.trim().split(" ").map(Number));
+const MOVES = [[1,0],[-1,0],[0,1],[0,-1]];
+
+
+function isRange(r, c) {
+    if(r<0 || r>=N || c<0 || c>=N) return false;
+    return true;
+}
+const NONE = 0;
+
+// function dfs(r, c, visited, newGrid2) {
+//     visited[r][c] = true;
+//     let cnt = 1;
+//     for(let i=0;i<4;i++) {
+//         const [ny, nx] = MOVES[i];
+//         const my = ny+r , mx = nx+c;
+//         if(!isRange(my, mx) || visited[my][mx]) continue;
+//         if(newGrid2[r][c] === newGrid2[my][mx]) cnt += dfs(my, mx, visited, newGrid2);
+//     }
+//     return cnt;
+// }
+let ret = -1;
+function bomb(r, c) {
+    const range = arr[r][c] -1;
+    const newGrid =  Array.from({length:N} ,() => new Array(N).fill(0));
+   
+    for(let i=0;i<N;i++)  {
+        for(let j=0;j<N;j++) {
+            newGrid[i][j] = arr[i][j];
+        }
+    }
+     newGrid[r][c] = NONE;
+    for(let i=0;i<4;i++) {
+        const [ny, nx] = MOVES[i];
+        for(let j=0;j<range;j++) {
+            const my = ny+r , mx = nx+c;
+            //  if(r=== 2&& c===1)console.log(my, mx)
+             if(!isRange(my, mx)) continue;
+            newGrid[my][mx] = NONE;
+        }
+    }
+    const newGrid2 = Array.from({length:N} ,() => new Array(N).fill(0));
+    //중력이 작동해야함
+    for(let i=0;i<N;i++) {
+        let idx = N-1;
+        for(let j=N-1;j>=0;j--) {
+            if(newGrid[j][i] > NONE) {
+                newGrid2[idx--][i] = newGrid[j][i];
+            }
+        }
+    }
+   const visited = Array.from({length:N} ,() => new Array(N).fill(false));
+    //dfs를 진행해서 카운팅 해야함.
+        //중력이 작동해야함
+    let cntNum = 0;
+    for(let i=0;i<N;i++) {
+        let n =-1, idx=0, cnt = 0;
+        for(let j=0;j<N;j++) {
+            if(n === newGrid[i][j]) {
+                cnt += 1;
+            } else {
+                if(cnt >= 2) cntNum+= 1;
+                n = arr[i][j];
+                idx=j;
+                cnt = 1;
+            }
+        }
+        if(cnt >=2) cntNum+=1;
+    }
+
+    for(let i=0;i<N;i++) {
+        let n =-1, idx=0, cnt = 0;
+        for(let j=0;j<N;j++) {
+            if(n === newGrid[j][i]) {
+                cnt += 1;
+            } else {
+                if(cnt >= 2) cntNum+= 1;
+                n = arr[j][i];
+                idx=j;
+                cnt = 1;
+            }
+        }
+        if(cnt >=2) cntNum+=1;
+    }
+
+    // if(r==2 && c==1) console.log(visited);
+    ret = Math.max(cntNum, ret);
+}
+
+function Solution() {
+    //완전 탐색으로 진행한다.
+    for(let i=0;i<N;i++) {
+        for(let j=0;j<N;j++) {
+            bomb(i, j);
+        }
+    }
+    console.log(ret);
+}
+
+Solution();
