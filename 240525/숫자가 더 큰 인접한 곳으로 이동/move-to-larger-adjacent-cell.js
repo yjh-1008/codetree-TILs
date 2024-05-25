667cbb8cf97abede076ce592223a6282ddd06a27
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-25 22:52:09 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 213ms, Memory: 15MB, Status: Passed - CodeTree

--- 240525/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.js
+++ 240525/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.js
@@ -4,7 +4,7 @@ let [n, r, c] = input[0].trim().split(" ").map(Number);
 const arr = input.slice(1, input.length).map((item) => item.trim().split(" ").map(Number));
 
 const DX = [0,0,-1,1];
-const DY = [1,-1,0,0];
+const DY = [-1,1,0,0];
 
 function isRange (r, c) {
     if(r < 0 || r>=n || c< 0 || c>=n) return false;
@@ -13,15 +13,13 @@ function isRange (r, c) {
 
 function Solution() {
     let ret = ``;
-
-    // let chk = true;
-    // let point
     r -= 1; c-=1;
     while(true) {
         let max = arr[r][c], nextY = -1,nextX = -1;
         ret += `${arr[r][c]} `
         for(let i=0;i<4;i++) {
             const my = DY[i]+ r, mx = DX[i]+c;
+            // console.log(my, mx, DY[i], DX[i]);
             if(!isRange(my, mx)) continue;
             if(max < arr[my][mx]) {
                 max = arr[my][mx];
