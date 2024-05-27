dff80390be91b79938ffc02a3edbf09859f5dd15
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-28 01:15:50 +0900

    Title: 대폭발, Time: 180ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240528/대폭발/big-explosion.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString();
+let [n, m, r, c] = input.trim().split(" ").map(Number);
+const arr = Array.from({length:n},() => new Array(n).fill(0));
+
+const dx = [0,0,-1,1];
+const dy = [1,-1,0,0];
+
+function isRagne(r, c) {
+    if(r < 0 || r>=n || c< 0 || c>=n) return false;
+    return true;
+}
+
+function dfs(r, c,visited, t) {
+     t= Math.pow(2, t-1)
+    for(let i=0;i<4;i++) {
+        const my = dy[i]*t + r, mx = dx[i]*t+c;
+        if(!isRagne(my,mx) || arr[my][mx] === 1) continue;
+        visited[my][mx] = true;
+        arr[my][mx] = 1;
+    }
+}
+
+
+function Solution() {
+    r-=1, c-=1;
+    arr[r][c] = 1;
+
+    for(let t=1;t<=m;t++) {
+        const visited = Array.from({length:n},() => new Array(n).fill(false));
+        for(let i=0;i<n;i++) {
+            for(let j=0;j<n;j++) {
+                if(arr[i][j] === 0 || visited[i][j] ) continue;
+                visited[i][j] = true;
+                dfs(i, j,visited, t);
+            }
+        }
+        // console.log(arr)
+    }
+    let ret = 0;
+    for(let i=0;i<n;i++) {
+        for(let j=0;j<n;j++) {
+            if(arr[i][j] === 1) ret+=1;
+        }
+    }
+    console.log(ret)
+}
+
+Solution();
