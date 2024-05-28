ec33d07dcafdee27ffafd9b1d4eff09c624f6148
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-28 22:55:13 +0900

    Title: 뱀은 사과를 좋아해, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240528/뱀은 사과를 좋아해/snake-loves-apples.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const input= fs.readFileSync(0).toString().trim().split("\n");
+const [N,M,K] = input[0].split(" ").map(Number);
+const arr = Array.from({length:N}, () => new Array(N).fill(0));
+
+function isRange(r, c) {
+    if(r < 0 || r>=N || c< 0 || c>=N) return false;
+    return true;
+}
+
+const dx = [0,0,-1,1];
+const dy = [-1,1,0,0];
+
+function Solution() {
+    for(let i=1;i<1+M;i++) {
+        const [r,c] = input[i].split(" ").map((v) => v-1);
+        arr[r][c] = 1;
+    }
+
+    const cmds = input.slice(1+M, input.length).map((item) => item.split(" "));
+    let snake = [[0,0]];
+    let ret = 0;
+    for(let t = 0;t<cmds.length;t++) {
+        const [dir, count] = cmds[t]
+        const idx = MOVE_OBJ[dir];
+        const my = dy[idx], mx = dx[idx];
+        for(let i=0;i<count;i++) {
+            const head = snake[0];
+            const ny = my+head[0], nx = mx+head[1];
+            console.log(snake)
+            if(!isRange(ny, nx) || snake.includes([ny, nx])) return ret;
+            //사과가 있다면 길이를 1늘림
+            if(arr[ny][nx] === 1) {
+                snake = [[ny,nx], ...snake];
+                arr[ny][nx] = 0;
+            } else {
+                // if(snake.length === 1) {
+                //     snake = [[ny, nx]]
+                // }
+                snake.pop();
+                snake = [[ny,nx], ...snake];
+            }
+            ret += 1;
+        }
+    }
+    return ret;
+}
+
+const MOVE_OBJ = {
+    'U': 0,
+    'D' : 1,
+    'L' : 2,
+    'R' : 3,
+}
+
+console.log(Solution());
