d0be29c8644713d4e377e4aad04d56a868d10caf
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-05-30 01:21:22 +0900

    Title: 핀볼게임, Time: 5080ms, Memory: 20MB, Status: Time Limit Exceed - CodeTree

--- 240530/핀볼게임/pinball-game.js
+++ 240530/핀볼게임/pinball-game.js
@@ -21,7 +21,7 @@ function go(r, c, idx) {
         r+=dy[idx], c+=dx[idx];
         time+=1;
    
-        if(time >= N*N) {
+        if(time >= 100000000) {
             return;
         }
         if(!isRagne(r, c)) {
