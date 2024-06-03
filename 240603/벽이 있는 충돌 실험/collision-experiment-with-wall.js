4cb39579cf72ad3b43e6f2cec1eedca4b98fbd87
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-06-03 23:01:25 +0900

    Title: 벽이 있는 충돌 실험, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240603/벽이 있는 충돌 실험/collision-experiment-with-wall.js
+++ 240603/벽이 있는 충돌 실험/collision-experiment-with-wall.js
@@ -25,62 +25,73 @@ function changeIdx(idx) {
     else return 2;
 }
 
-function move(arr,cnt) {
-    let time = 0;
-    while(true) {
-        if(cnt === 1 || time === N *2) break;
-        time += 1;
-        const nextGrid = Array.from({length:N},( )=> new Array(N).fill(-1));
-        // console.log(arr)
-        for(let i=0;i<N;i++) {
-            for(let j=0;j<N;j++) {
-                //해당 위치에 공이 있다면
-                if(arr[i][j] > -1) {
-                    let idx = arr[i][j];
-                    let ny = i+dy[idx], nx = j+dx[idx];
-                    if(!isRange(ny, nx)) {
-                        idx = changeIdx(idx);
-                        if(nextGrid[i][j] > -1) {
-                            cnt -=2;
-                            nextGrid[i][j] = -1;
-                        }else {
-                            nextGrid[i][j] = idx;
-                        }
-                        continue;
-                    }
-                    //해당 위치에 공이 있다면?
-                    if(nextGrid[ny][nx] > -1) {
-                        nextGrid[ny][nx] = -1;
-                        cnt -= 2;
-                    } else { //없다면 공을 채운다.
-                        nextGrid[ny][nx] = idx;
-                    }
-                }
-            }
-        }
-
-        arr = nextGrid
-
+function move(marble) {
+    // 구슬이 벽에 부딪혔을 때의 처리를 간단히 하기 위해
+    // dir 기준 0, 3이 대칭 1, 2가 대칭이 되도록 설정합니다.
+    const dx = [-1, 0, 0, 1], dy = [0, 1, -1, 0];
+    let [x, y, moveDir] = marble;
+    
+    // 바로 앞에 벽이 있는지를 판단합니다.
+    const nx = x + dx[moveDir], ny = y + dy[moveDir];
+    
+    // Case 1 : 벽이 없는 경우에는 그대로 한 칸 전진합니다.
+    if (isRange(nx, ny)) {
+        return [nx, ny, moveDir];
+    } 
+    // Case 2 : 벽이 있는 경우에는 방향을 반대로 틀어줍니다.
+    // 위에서 dx, dy를 move_dir 기준 0, 3이 대칭 1, 2가 대칭이 되도록
+    // 설정해놨기 때문에 간단하게 처리가 가능합니다.
+    else {
+        return [x, y, 3 - moveDir];
     }
-    return cnt;
 }
 
+
+function moveAll() {
+    marbles.forEach((marble, idx) => {
+        marbles[idx] = move(marble);
+    })
+}
+function duplicateMarbleExist(targetIdx) {
+    const [targetX, targetY, _] = marbles[targetIdx];
+    
+    for (let i = 0; i < marbles.length; i++) {
+        const [x, y, _] = marbles[i];
+        if (i !== targetIdx && x === targetX && y === targetY) {
+            return true;
+        }
+  }
+  return false;
+}
+
+
+
+function removeDuplicateMarbles() {
+    marbles = marbles.filter((_, i) => !duplicateMarbleExist(i));
+}
+
+
+function simulate() {
+    moveAll();
+    removeDuplicateMarbles();
+}
+let marbles = [];
 function Solution() {
     let ret = '';
     while(T) {
        const tmp = input[fsIdx++].trim().split(" ").map(Number); 
        N = tmp[0], M = tmp[1];
        const arr = Array.from({length:N},()=> new Array(N).fill(-1));
-        let cnt = 0;
-        const marbles = [];
+        marbles = [];
         for(let i=0;i<M;i++) {
             const [y,x,d] = input[fsIdx++].trim().split(" ");
             arr[y-1][x-1] = MOVE_OBJ[d];
-            // marbles.push([y-1, x])
+            marbles.push([y-1, x-1, MOVE_OBJ[d]])
             cnt+=1;
         }
-
-        ret += move(arr,cnt) + '\n';
+        for (let i = 0; i < 2 * n; i++)
+            simulate();
+        ret += marbles.length + '\n';
         T--;
     }
     console.log(ret)
