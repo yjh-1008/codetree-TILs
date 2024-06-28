ee70cc04a334c1cfd262bc08041a747f355f708a
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-06-28 23:51:51 +0900

    Title: 1차원 윷놀이, Time: 160ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240628/1차원 윷놀이/yutnori-1d.js
+++ 240628/1차원 윷놀이/yutnori-1d.js
@@ -5,28 +5,32 @@ const map = input[1].split(" ").map(Number);
 let ret = -1;
 function go(cnt, arr) {
     if(cnt === n) {
+        // console.log(arr);
         let cnt = 0
         for(let i=0;i<arr.length;i++) {
-            if(arr[i] >= m) {
+            if(arr[i] >= m-1) {
                 cnt++
             }
         }
         ret = Math.max(cnt, ret);
         return;
     }
-
+    // console.log(arr);
     arr.forEach((item, index) => {
-        if(item < m) {
+        // console.log(item)
             arr[index] += map[cnt];
-            go(cnt+1, arr);
+            // console.log(arr)
+            if(arr[index] < m) go(cnt+1, arr);
             arr[index] -= map[cnt];
+            go(cnt+1, arr);
         }
-    })
+    )
+
 }
 
 function Solution() {
+    // console.log(k)
     const arr = new Array(k).fill(0);
-    console.log
     go(0, arr);
     console.log(ret)
 }
