73a2a1fab8d81798842c4b75eabcc4aadc20dc29
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-07-19 01:30:10 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 220ms, Memory: 18MB, Status: Passed - CodeTree

--- 240719/수들 중 최솟값 최대화하기/maximin-of-numbers.js
+++ 240719/수들 중 최솟값 최대화하기/maximin-of-numbers.js
@@ -3,22 +3,24 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const N = Number(input[0]);
 const arr = input.slice(1, input.length).map(item => item.split(" ").map(Number));
 let ret = Number.MIN_VALUE;
-const visited = Array(N).fill(false);
-
-function go(r, tmp) {
+const visited = new Array(N).fill(false);
+let tmp = [];
+function go(r) {
     if(r === N) {
         if(tmp.length !== N) return;
-        // console.log(tmp)
-        const max = tmp.sort((a, b) => a-b)[0];
+        let max = Number.MAX_VALUE
+        for(let i = 0;i<N;i++) {
+            max = Math.min(max, arr[i][tmp[i]])
+        }
         
         ret = Math.max(ret, max);
         return;
     }
 
     for(let i=0;i<N;i++) {
-        if(visited[i]) continue;
+        if(visited[i] === true) continue;
         visited[i] = true;
-        tmp.push(arr[r][i]);
+        tmp.push(i);
         go(r+1, tmp);
         tmp.pop();
         visited[i]= false;
@@ -27,7 +29,7 @@ function go(r, tmp) {
 
 function Solution() {
     // console.log(arr)
-    go(0, []);
+    go(0);
     console.log(ret)
 }
 
