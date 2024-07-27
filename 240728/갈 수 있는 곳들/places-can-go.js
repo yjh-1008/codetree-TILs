871c520444d0cb5d70df905a7fe76778f1eeb8a5
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-07-28 01:54:02 +0900

    Title: 갈 수 있는 곳들, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240728/갈 수 있는 곳들/places-can-go.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [N, K] = input[0].split(" ").map(Number);
+const arr = input.slice(1, 1+N).map((item) => item.split(" ").map(Number));
+const visited = Array.from({length:N}, () => Array(N).fill(false));
+for(let i=0;i<N;i++) {
+    for(let j=0;j<N;j++) {
+        if(arr[i][j]) visited[i][j] = true;
+    }
+}
+const dr = [-1,1,0,0];
+const dc = [0,0,-1,1];
+let ret = 0;
+
+function moveable(nr, nc) {
+    if(nr < 0 || nr>N-1 || nc<0 || nc > N-1) return false;
+    return true;
+}
+
+function Solution() {
+    input.slice(1+N, input.length).map((item) => {
+        const [r, c] = item.split(" ").map(Number);
+        if(visited[r][c] === false) {
+            const q = [[r, c]];
+            visited[r][c] = true;
+            let cnt = 1;
+            while(q.length) {
+                const [cr, cc] = q.shift();
+
+                for(let i=0;i<4;i++) {
+                    const nr = dr[i] + cr, nc = dc[i] + cc;
+                    if(moveable(nr, nc)) {
+                        if(visited[nr][nc]=== true) continue;
+                        visited[nr][nc] = true;
+                        q.push([nr, nc]);
+                        cnt+=1;
+                    }
+                }
+            }
+            ret += cnt;
+        }
+
+    })
+    console.log(ret);
+}
+
+Solution();
