21233c3a73ae9c9135b1d2acdb64f7d7fb3cf485
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-07-31 02:03:34 +0900

    Title: 계단 오르기, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240731/계단 오르기/climbing-stairs.js
+++ 240731/계단 오르기/climbing-stairs.js
@@ -7,7 +7,9 @@ const arr = Array(1001).fill(0);
 function go(cur) {
     if(cur === 0) return 1;
     else if(cur < 0 ) return 0;
-    // else return arr[n];
+    
+    if(arr[n] !== 0) return arr[n];
+    
     arr[n] = go(cur-2) + go(cur-3);
     return arr[n];
 }
