252ec3f6a51a0ffb150dff4e22659fceeb4334df
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-07-31 01:36:03 +0900

    Title: 피보나치 수, Time: 163ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240731/피보나치 수/fibonacci-number.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const N = +input[0];
+const arr = new Array(46).fill(-1);
+function fibo(n) {
+    if(arr[n] !== -1) {
+        return arr[n];
+    }
+
+    if(n <= 2) {
+        return 1;
+    }
+
+    arr[n] = fibo(n-1) + fibo(n-2);
+
+    
+}
+
+for(let i=1;i<=N;i++) {
+    arr[i] = fibo(i);
+}
+
+console.log(arr[N])
