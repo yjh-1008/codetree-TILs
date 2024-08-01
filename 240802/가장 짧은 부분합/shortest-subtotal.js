d7f648a2349f6abcf47a45e1606b00955963f7e8
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-02 00:08:58 +0900

    Title: 가장 짧은 부분합, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240802/가장 짧은 부분합/shortest-subtotal.js
+++ 240802/가장 짧은 부분합/shortest-subtotal.js
@@ -5,20 +5,34 @@ const arr = input[1].split(" ").map(Number);
 
 let lt = 0, rt = 0;
 let ret =Number.MAX_VALUE
-
 let sum = 0;
 let j = 0;
-for(let i=0;i<N;i++) {
-    // sum += arr[i];
-    while(true) {
-        if(j>N || sum >= S) break;
-        sum += arr[j++];
+
+while(rt<N) {
+    if(sum < S) {
+        sum += arr[rt++];
+    } else {
+        sum -= arr[lt++];
+        ret = Math.min(rt-lt);
     }
-    // console.log(sum);
-    if(j <= N) {
-        ret = Math.min(j-i, ret);
-    }
-    sum -= arr[i];
 }
+
+while(sum > S && lt < N) {
+        sum -= arr[lt++];
+        ret = Math.min(rt-lt);
+}
+
+// for(let i=0;i<N;i++) {
+//     // sum += arr[i];
+//     while(true) {
+//         if(j>N || sum >= S) break;
+//         sum += arr[j++];
+//     }
+//     // console.log(sum);
+//     if(j <= N) {
+//         ret = Math.min(j-i, ret);
+//     }
+//     sum -= arr[i];
+// }
 
 console.log(ret === Number.MAX_VALUE ? -1 : ret);
