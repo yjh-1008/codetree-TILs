6fe1d6f45081f104af11af300eede2e32dce52be
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-02 01:12:14 +0900

    Title: 가장 짧은 부분합, Time: 220ms, Memory: 23MB, Status: Passed - CodeTree

--- 240802/가장 짧은 부분합/shortest-subtotal.js
+++ 240802/가장 짧은 부분합/shortest-subtotal.js
@@ -3,23 +3,33 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const [N, S] = input[0].split(" ").map(Number);
 const arr = input[1].split(" ").map(Number);
 
-let lt = 0, rt = 0;
+let lt = 0, rt = 1;
 let ret =Number.MAX_VALUE
-let sum = 0;
+let sum = arr[0];
 let j = 0;
 
 while(rt<N) {
+    if(lt === rt) {
+        sum += arr[rt++]
+        continue
+    }
     if(sum < S) {
         sum += arr[rt++];
     } else {
+         ret = Math.min(rt-lt, ret);
         sum -= arr[lt++];
-        ret = Math.min(rt-lt+1);
+      
     }
 }
 
 while(sum > S && lt < N) {
+    if(sum >= S) {
+        ret = Math.min(rt-lt, ret);
+    }
     sum -= arr[lt++];
-    ret = Math.min(rt-lt+1);
+    // console.log(sum)
+
+   
 }
 
 // for(let i=0;i<N;i++) {
