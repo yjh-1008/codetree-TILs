230378d1c71785e3c78ca9ff765e01619f7305bb
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-03 00:03:25 +0900

    Title: 좌우 반전시키기, Time: 5065ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

---
+++ 240803/좌우 반전시키기/flip-left-and-right.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const input= fs.readFileSync(0).toString().trim().split("\n");
+
+const N = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+let ret = Number.MAX_VALUE;
+
+const reverse = (n) => {
+    return n ^= 1;
+}
+
+const go = (idx, cnt) => {
+    if(cnt >= ret) return;
+    if(idx === N) {
+        // let chk = true;
+        const idx = arr.findIndex((item) => item === 0);
+        if(idx === -1) {
+            ret = Math.min(ret, cnt);
+        }
+        return;
+    }
+
+
+    const tmp = [...arr];
+
+    arr[idx] = reverse(arr[idx]);
+    if(idx-1 >= 0) arr[idx-1] = reverse(arr[idx-1]);
+    if(idx+1 < N) arr[idx+1] = reverse(arr[idx+1]);
+
+    go(idx+1, cnt+1);
+    // go(idx, cnt);
+    arr = tmp;
+    go(idx+1, cnt);
+    // go(idx, cnt)
+}
+
+function Solution() {
+    go(1, 0);
+    console.log(ret === Number.MAX_VALUE ? -1 : ret);
+}
+
+Solution();
