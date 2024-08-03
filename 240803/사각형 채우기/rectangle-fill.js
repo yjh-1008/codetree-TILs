82ab6088f2ebda55258d22e7864402963e5b7d8c
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-03 17:59:55 +0900

    Title: 사각형 채우기, Time: 200ms, Memory: 47MB, Status: Passed - CodeTree

--- 240803/사각형 채우기/rectangle-fill.js
+++ 240803/사각형 채우기/rectangle-fill.js
@@ -6,7 +6,7 @@ const dp = Array(N+1).fill(0);
 dp[0] = 1;
 dp[1] = 1;
 for(let i=2;i<=N;i++) {
-    dp[i] = dp[i-2] + dp[i-1];
+    dp[i] = (dp[i-2] + dp[i-1])%10007;
     // console.log(dp[i])
 }
 // console.log(N)
