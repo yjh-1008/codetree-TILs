a2271945930338db74a5e01bd8c0fe9a22f09984
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-03 17:59:14 +0900

    Title: 사각형 채우기, Time: 166ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240803/사각형 채우기/rectangle-fill.js
+++ 240803/사각형 채우기/rectangle-fill.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
-const N = Number(input[0]);
+const N = Number(input);
 
 const dp = Array(N+1).fill(0);
 dp[0] = 1;
 dp[1] = 1;
 for(let i=2;i<=N;i++) {
     dp[i] = dp[i-2] + dp[i-1];
+    // console.log(dp[i])
 }
-
+// console.log(N)
 console.log(dp[N])
