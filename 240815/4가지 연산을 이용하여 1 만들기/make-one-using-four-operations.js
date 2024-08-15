d79f9f14f93d0d860d813a36e19daf7bf54aa375
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-15 15:47:55 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 242ms, Memory: 30MB, Status: Wrong Answer - CodeTree

---
+++ 240815/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+class Queue {
+    constructor() {
+        this.q = [];
+        this.head = 0;
+        this.tail = 0;
+    }
+
+    push(val) {
+        this.q[this.head++] = val;
+    }
+
+    pop() {
+        const ret = this.q[this.tail++];
+        if(this.head === this.tail) {
+            this.head = 0;
+            this.tail = 0;
+        }
+        return ret
+    }
+
+    length() {
+        return this.head - this.tail;
+    }
+} 
+const MAX = 1000010
+const N = Number(input);
+const step = Array(MAX).fill(Number.MAX_SAFE_INTEGER);
+
+function moveable(n) {
+    return true
+}
+
+function Solution(){
+    const q = new Queue();
+    q.push(N);
+    step[N] = 0;
+    while(q.length()) {
+        const cur = q.pop();
+        // console.log(cur)
+        if(cur === 1) continue;
+        if(moveable(cur-1) && step[cur-1] > step[cur]-1) {
+            step[cur-1] = step[cur]+1
+            q.push(cur-1)
+        }
+
+        if(moveable(cur+1) && step[cur+1] > step[cur]+1) {
+            step[cur+1] = step[cur]+1
+            q.push(cur+1)
+        }
+
+
+        if(cur %2 === 0 &&step[cur/2] > step[cur+1]) {
+            step[cur/2] = step[cur]+1;
+            q.push(cur/2)
+        }
+
+         if(cur %3 === 0 && step[cur/3] > step[cur]+1) {
+            step[cur/3] = step[cur]+1
+            q.push(cur/3)
+        }
+    }
+    console.log(step[1])
+}
+
+Solution();
