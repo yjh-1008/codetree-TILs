2c72be5f9504a88ffea6dcb68d1ea13d788346e3
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-17 15:34:39 +0900

    Title: 사각형 채우기 3, Time: 188ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240817/사각형 채우기 3/rectangle-fill-3.js
+++ 240817/사각형 채우기 3/rectangle-fill-3.js
@@ -15,14 +15,14 @@ const input = fs.readFileSync(0).toString().trim();
 const N = Number(input);
 
 const dp = Array(N+1).fill(0);
+dp[0] = 1;
 dp[1] = 2;
 dp[2] = 7;
 const MOD = 1000000007
 for(let i=3;i<=N;i++) {
-    if(i %2 === 0) {
-        dp[i] = ((dp[i-1]*2) + dp[i-1] + dp[i-2] - 2) % MOD;
-    } else {
-        (dp[i] = (dp[i-1]*2) + dp[i-1] + dp[i-2] - 1) % MOD
+    dp[i] = (dp[i-2] *3 + dp[i-1] * 2) % MOD;
+    for(let j=i-3;j>=0;j--) {
+        dp[i] += (dp[j] * 2) % MOD
     }
 }
 
