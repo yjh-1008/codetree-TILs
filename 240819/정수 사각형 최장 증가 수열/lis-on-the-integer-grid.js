f8dfbd6dea742bced6d371caad8842e540a2e4d0
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-19 22:56:29 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 273ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240819/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.js
+++ 240819/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.js
@@ -4,7 +4,7 @@ const N = Number(input[0]);
 const arr = input.slice(1, input.length).map((item) => {
     return item.split(" ").map(Number);
 })
-const dp = Array.from({length:N}, () => Array(N).fill(0));
+const dp = Array.from({length:N}, () => Array(N).fill(1));
 
 function moveable(r, c) {
     return r >= 0 && r < N && c>=0 && c<N;
@@ -18,25 +18,21 @@ function go(r, c) {
     const dc = [0,0,-1,1];
     for(let i=0;i<4;i++) {
         const nr = dr[i] +r, nc = dc[i] + c;
-        if(moveable(nr, nc) && arr[r][c] < arr[nr][nc] )  {
+        if(moveable(nr, nc) && arr[r][c] < arr[nr][nc])  {
             dp[nr][nc] = Math.max(dp[r][c] +1, dp[nr][nc])
-            go(nr, nc);
+            
         }
     }
 }
 
 function Solution() {
     //이미 밟은 공간이라면 갈 수 있는 최대 범위를 간것.
-    //dp[i][j] = Math.max(dp[x][y]+1, dp[i][j]);
-
+    //dp[i][j]의 값은 여태까지 진입했던 모든 카운트의 최대값.
+    //점화식 = Math.max(이전 발판의 카운트 + 1, 현재 발판의 값)
     for(let i=0;i<N;i++) {
         for(let j=0;j<N;j++) {
-            if(dp[i][j] === 0) {
-                dp[i][j] = 1;
                 go(i, j);
-            }
         }
-        // console.log(dp);
 
     }
 
@@ -46,6 +42,7 @@ function Solution() {
             if(max < dp[i][j]) max = dp[i][j]
         }
     }
+    // console.log(dp)
     console.log(max);
 }
 
