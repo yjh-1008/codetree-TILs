96cca5ddb8978782337346ef87b438be54276d69
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-24 01:09:22 +0900

    Title: 최대 증가 부분 수열, Time: 214ms, Memory: 47MB, Status: Passed - CodeTree

--- 240824/최대 증가 부분 수열/longest-increasing-subsequence.js
+++ 240824/최대 증가 부분 수열/longest-increasing-subsequence.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split("\n");
 const N = Number(input[0]);
 const arr = input[1].split(" ").map(Number);
-const dp = Array(N).fill(0);
+const dp = Array(N).fill(1);
 function Solution() {
-    dp[0] = 1;
+    // dp[0] = 1;
     for(let i=1;i<N;i++) {
         for(let j=0;j<i;j++) {
             if(arr[i] > arr[j] && dp[i] < dp[j]+1) {
@@ -12,8 +12,8 @@ function Solution() {
             }
         }
     }
-
-    console.log(dp[N-1])
+    console.log(Math.max(...dp))
+    // console.log(dp[N-1])
 }
 
 Solution()
