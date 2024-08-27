d82b8990a3bc402332e0f4e14c73101c0c688127
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-27 23:20:26 +0900

    Title: 최대 점프 횟수, Time: 207ms, Memory: 47MB, Status: Passed - CodeTree

--- 240827/최대 점프 횟수/maximum-number-of-jumps.js
+++ 240827/최대 점프 횟수/maximum-number-of-jumps.js
@@ -3,11 +3,12 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const N = Number(input[0]);
 
 const arr = input[1].split(" ").map(Number);
-const dp = Array(N).fill(0);
+const dp = Array(N).fill(-1);
 function Solution() {
     //N칸 점프하는 동안 dp[i+j] = Math.max(dp[i+j], dp[i]+1);
-    
+    dp[0] = 0
     for(let i=0;i<N;i++) {
+        if(dp[i] === -1) continue;
         for(let j=1;j<=arr[i];j++) {
             if(i+j >= N) continue;
             if(dp[i+j] < dp[i]+1) {
@@ -15,7 +16,7 @@ function Solution() {
             }
         }
     }
-    console.log(dp[N-1])
+    console.log(Math.max(...dp))
 }
 
 Solution();
