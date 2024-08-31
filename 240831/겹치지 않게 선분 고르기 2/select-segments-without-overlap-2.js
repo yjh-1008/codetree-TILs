cf980518a017c62ce99f5c3c021a73887f8fcc61
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-08-31 18:51:41 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 244ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240831/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const N = Number(input[0]);
+const arr = input.slice(1, input.length).map((v) => {
+    return v.split(" ").map(Number);
+});
+const dp = Array(1001).fill(-1);
+
+// function go(idx) {
+//     const [r, c] = arr[idx];
+
+//     for(let j=)
+// }
+
+function Solution() {
+    arr.sort((a,b) => {
+        if(a[0] === b[0]) return a[1] - b[1];
+        return a[0] - b[0];
+    })
+
+    dp[0] = 1;
+
+    for(let i=1;i<N;i++) {
+        for(let j=0;j<i;j++) {
+            if(arr[j][1] <= arr[i][0]) {
+                if(dp[i] < dp[j]+1) {
+                    dp[i] = dp[j]+1;
+                }
+            }
+        }
+    }
+    console.log(Math.max(...dp));
+}
+
+Solution();
