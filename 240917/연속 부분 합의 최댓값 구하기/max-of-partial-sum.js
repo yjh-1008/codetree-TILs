42d9e92ec989a2f6942be36b2c1e9065c5fbe741
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-09-17 22:19:01 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 246ms, Memory: 47MB, Status: Passed - CodeTree

--- 240917/연속 부분 합의 최댓값 구하기/max-of-partial-sum.js
+++ 240917/연속 부분 합의 최댓값 구하기/max-of-partial-sum.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split("\n");
 const N = +input[0];
 const arr = [0,...input[1].split(" ").map(Number)];
-const dp = Array(N+1).fill(-1)
+const dp = Array(N+1).fill(Number.MIN_SAFE_INTEGER)
 
 const Solution = () => {
-    dp[0] = 0;
+    // dp[0] = Number.MIN_SAFE_INTEGER;
     dp[1] = arr[1];
 
     //dp : i번째 이하의 속한 연속된 부분수열 중 최대의 값.
