aca022c23b370661df07e1958ace9a7627b9a25a
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-09-22 17:39:51 +0900

    Title: 올바른 등식 만들기, Time: 276ms, Memory: 18MB, Status: Passed - CodeTree

--- 240922/올바른 등식 만들기/right-equality.js
+++ 240922/올바른 등식 만들기/right-equality.js
@@ -1,32 +1,28 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split("\n");
 const [N,M] = input[0].split(" ").map(Number);
-const arr = input[1].split(" ").map(Number);
+const arr = [0].concat(input[1].split(" ").map(Number));
 const dp = Array.from({length:N+1} , () => Array(41).fill(0n));
 
 //더하거나 뺀다
 const solution = () => {
-    // dp[0][20] = 1;
-    //i-1번째에 더하거나 뺀 수에 더하거나 뺏을때 M인가?
-    const isRange = (num) => {
-        return num >= 0 && num < 41;
-    }
-    dp[0][arr[0]+ 20] = 1n;
-    dp[0][-1*arr[0] + 20] = 1n;
+    dp[0][20] = 1n;
+    // dp[0][-1*arr[0] + 20] = 1n;
     for(let i=1;i<=N;i++) {
         for(let j=0;j<41;j++) {
-            if(dp[i-1][j] === -1) continue;
+            // if(dp[i-1][j] === -1) continue;
             
-            if(isRange(j-arr[i])) {
+            if(j-arr[i] >= 0) {
+                // console.log('here')/
                 dp[i][j] += dp[i-1][j-arr[i]];
             }
-            if(isRange(j+arr[i])) {
+            if(j+arr[i] <= 40) {
                 dp[i][j] += dp[i-1][j+arr[i]];
             }
         }
     }
     
-    console.log(dp[N-1][M+20])
+    console.log(dp[N][M+20].toString())
 }
 
 solution();
