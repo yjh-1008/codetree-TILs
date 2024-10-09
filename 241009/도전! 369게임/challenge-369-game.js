6150578a007636704d5bffb9caf4cbe6db7bbff4
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2024-10-09 17:42:41 +0900

    Title: 도전! 369게임, Time: 5036ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 241009/도전! 369게임/challenge-369-game.js
+++ 241009/도전! 369게임/challenge-369-game.js
@@ -4,8 +4,14 @@ const N = +fs.readFileSync(0).toString()
 const Solution = () => {
     let answer = 0;
     for(let i=1;i<=N;i++) {
-        if(i%3 === 0) answer++;
-        else if(i.toString().includes('3')) answer++
+        if(i%3 === 0) {
+            // console.log(i)
+            answer++;
+        }
+        else if(i.toString().includes('3') || i.toString().includes('6')|| i.toString().includes('9')) {
+            //  console.log(i)
+            answer++
+        }
     }
 
     console.log(answer)
