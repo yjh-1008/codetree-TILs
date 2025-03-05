f3d61da7ab7b530a8bcc6d0612dc96bcfce7df14
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2025-03-05 21:32:50 +0900

    Title: 최장 공통 부분 수열, Time: 256ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 250305/최장 공통 부분 수열/longest-common-sequence.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const str1 = input[0];
+const str2 = input[1];
+const dp = Array.from({length:str1.length+1}, () => {
+    return Array(str2.length+1).fill(0);
+})
+// Please write your code here.
+function Solution() {
+    dp[1][1] = str1[0] === str2[0] ? 1 : 2;
+
+    for(let i=1;i<=str1.length;i++) {
+        for(let j=1;j<=str2.length;j++) {
+            if(str1[i-1] === str2[j-1]) {
+                dp[i][j] = dp[i-1][j-1]+1
+            } else {
+                dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+            }
+        } 
+    }
+
+    return dp[str1.length][str2.length];
+}
+
+const ret = Solution();
+console.log(ret)
